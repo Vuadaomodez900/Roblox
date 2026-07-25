586b32b4f12c0aed71c09d63b1493b87d782a8ac
Vuadaomodez900 <vainonlol@gmail.com>
2026-07-25 13:09:35 +0700

    Update Load.lua to use Fixautoshoot
    Replaced Autoshoot script with Fixautoshoot script.

--- Load.lua
+++ Load.lua
@@ -1,6 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Speed%2Bbunnyhop.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Silentaim.lua"))()
-loadstring(/game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Autoshoot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Fixautoshoot.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Esp.lua"))()
 
 print("[Onyx v67] Arsenal Mobile Suite — ALL LOADED")
