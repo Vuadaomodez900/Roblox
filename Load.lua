4e495952ab714e55533bd62392aea4f812cf20e5
Vuadaomodez900 <vainonlol@gmail.com>
2026-07-25 12:47:52 +0700

    Add loading of external game scripts
    Load external scripts for game enhancements and print status messages.

---
+++ Load.lua
@@ -0,0 +1,8 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Speed%2Bbunnyhop.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Silentaim.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Autoshoot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vuadaomodez900/Roblox/refs/heads/main/Esp.lua"))()
+
+print("[Onyx v67] Arsenal Mobile Suite — ALL LOADED")
+print("Silent Aim | AutoShoot | Speed | BunnyHop | ESP")
+print("6767 gng")
